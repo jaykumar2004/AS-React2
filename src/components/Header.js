7631d27fb6ee0a66587d4fffd6a932b5f2b347af
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-09-15 16:52:24 +0530

    Login and Logout toggle functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
-import { LOGO_URL } from "../utils/constants";
+import {LOGO_URL} from "../utils/constants";
+import {useState} from "react";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
-      </div>
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contact us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
+
+    const [btnName, setBtnName] = useState("Login")
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL}/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                    <button className="login" onClick={() => {
+                        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
+                    }}>{btnName}</button>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
