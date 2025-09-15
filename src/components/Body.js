587c912dcaa0678cf166ad4c977964589f419719
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-09-15 16:27:37 +0530

    Use the live api and add the shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,34 @@
-import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 
-        const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+        const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+        useEffect(() => {
+            fetchData();
+        }, [])
+        const fetchData = async () => {
+            const data = await fetch(
+                "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.77508727955646&lng=75.85139371454716&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            );
+
+            const json = await data.json();
+
+            // Extract restaurant data properly
+            const restaurants =
+                json?.data?.cards?.find(
+                    (card) => card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+                )?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+
+            setListOfRestaurants(restaurants);
+        };
+
+        //conditional rendering
+        if (listOfRestaurants.length === 0) return (
+            <Shimmer/>
+        )
 
         return (
             <div className="body">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
 export const LOGO_URL =
-  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=restaurant-food&sf=&txt_keyword=All";
+    "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=restaurant-food&sf=&txt_keyword=All";
 
-
+export const API_KEY = "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.77508727955646&lng=75.85139371454716&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
