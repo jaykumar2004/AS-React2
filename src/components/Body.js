03c33335c102e701596a498cd9330560ffe772db
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-09-15 16:30:46 +0530

    conditional rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -26,11 +26,11 @@ const Body = () => {
         };
 
         //conditional rendering
-        if (listOfRestaurants.length === 0) return (
-            <Shimmer/>
-        )
+        // if (listOfRestaurants.length === 0) return (
+        //     <Shimmer/>
+        // )
 
-        return (
+        return listOfRestaurants.length === 0 ? <Shimmer/> : (
             <div className="body">
                 <div className="filter">
                     <button className="filter-btn" onClick={() => {
